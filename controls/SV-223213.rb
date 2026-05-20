13a484a4b8c2e79a764ba458761ba03593316991
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2026-05-20 16:14:27 -0400

    Update SV-223213.rb

--- controls/SV-223213.rb
+++ controls/SV-223213.rb
@@ -42,8 +42,8 @@ set system login class <class name> deny-commands "(start shell)"'
   # For each non-root login class, verify it denies the 'start shell' command
   describe 'Non-root login classes' do
     if non_root_classes.empty?
-      describe "listed here: \n#{non_root_classes}" do
-        it "should be empty" do
+      describe "listed here: #{non_root_classes}\n" do
+        it 'should be empty' do
           expect(non_root_classes).to be_empty, "List of login classes assigned to non-root users is not empty."
         end
       end
